fc7eb42f973d46332c6912f7e13e81c425eb6fa3
HassanJbara <hassan.jbara@singular-it.de>
2023-01-13 15:36:32 +0100

    configure server for development

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 const express = require("express");
 const serveStatic = require("serve-static");
+const compression = require("compression");
 const path = require("path");
 const app = express();
-app.use(serveStatic(path.join(__dirname, "dist")));
+
+app.use(compression()); // <-- use the library
+
+app.use("/", serveStatic(path.join(__dirname, "/dist")));
+
+app.get(/.*/, function(req, res) {
+  res.sendFile(path.join(__dirname, "/dist/index.html"));
+});
+
 const port = process.env.PORT || 80;
 app.listen(port);
+console.log(`app is listening on port: ${port}`);
